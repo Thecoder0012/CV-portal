f821f814877132035ff6a9ba8d77c1088d75c098
Battals <batt0006@stud.kea.dk>
2023-11-15 10:48:02 +0100

    Initial commit

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import session from "express-session"
 
 const app = express();
 
@@ -6,6 +7,11 @@ app.get("/",(req,res) => {
     res.send("Hello world");
 })
 
+
 app.listen(8080,() => {
     console.log("Running on port",8080);
 });
+
+
+
+
