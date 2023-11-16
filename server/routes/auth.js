05c762c68a7c370f3a38ce344fbb1e963154f021
Mardin <mardineliassi57@gmail.com>
2023-11-16 13:51:02 +0100

    create user and login/logout

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,32 @@ import bcrypt from "bcrypt";
 
 const router = Router();
 
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password, email, roleId } = req.body;
+    const encryptedPass = await bcrypt.hash(password, 12);
+    const existingUser = await db.query(
+      "SELECT * FROM users WHERE email = ? OR username = ?",
+      [email, username]
+    );
+    const [user] = existingUser[0];
+
+    if (user) {
+      return res
+        .status(409)
+        .send("An account already exists with this email/username");
+    } else {
+      const signUp = await db.query(
+        "INSERT into users (username,password,email,role_id) values (?,?,?,?)",
+        [username, encryptedPass, email, roleId]
+      );
+      return res.status(200).send("You have now signed up");
+    }
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
+});
+
 router.post("/login", async (req, res) => {
   try {
     const { username, password, email } = req.body;
