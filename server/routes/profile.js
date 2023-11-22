92a87cfdaed3781b5ac9bc5673d775f8f0b8082f
Mardin <mardineliassi57@gmail.com>
2023-11-22 18:30:13 +0100

    Create employee person

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -5,12 +5,23 @@ const router = Router();
 
 router.post("/profile", async (req, res) => {
     try {
-      const { first_name, last_name, date_of_birth, phone_number, address_id } = req.body;
+      const { first_name, last_name, date_of_birth, phone_number, address_id,user_id,department_id, project_id} = req.body;
       const createProfile = await db.query(
         "INSERT INTO person (first_name, last_name, date_of_birth, phone_number, address_id) VALUES (?,?,?,?,?)",
         [first_name, last_name, new Date(date_of_birth), phone_number, address_id]
       );
-      return res.status(200).send({message:"The person has been registered"});
+
+      const createEmployee = await db.query(
+        "INSERT INTO employee (person_id, user_id, department_id, skills_id) VALUES (?,?,?,?)",
+        [
+          createProfile[0].insertId,
+          user_id,
+          department_id,
+          project_id
+        ]
+      );
+
+      return res.status(200).send({message:"The employee has been registered"});
 
     } catch (error) {
       return res.status(500).send({message:"Internal server error"});
