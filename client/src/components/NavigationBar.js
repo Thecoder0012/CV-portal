9f5c3a9ba0131aca27bcf7f6a79b5c070d2f09a3
Mardin <mardineliassi57@gmail.com>
2023-12-03 02:48:23 +0100

    format

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,129 +1,126 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import { API_URL } from "../config/apiUrl.js";
-import logo from "../images/logo.png"
-import Navbar from "../styles/navBar.module.css"
+import logo from "../images/logo.png";
+import Navbar from "../styles/navBar.module.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faCaretDown} from  "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 export const NavigationBar = () => {
-    const [dropdownVisible, setDropdownVisible] = useState(false);
-    const [auth, setAuth] = useState();
-    const [role, setRole] = useState();
-    const [userId, setUserId] = useState()
-    const WITH_CREDENTIALS = { withCredentials: true };
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [auth, setAuth] = useState();
+  const [role, setRole] = useState();
+  const [userId, setUserId] = useState();
+  const WITH_CREDENTIALS = { withCredentials: true };
 
+  async function fetchUser() {
+    const response = await axios.get(API_URL + "/auth-login", WITH_CREDENTIALS);
 
-    async function fetchUser() {
-        const response = await axios.get(API_URL + "/auth-login", WITH_CREDENTIALS);
+    const userRole = response.data.user.role_id === 1 ? "Manager" : "Employee";
+    setRole(userRole);
+  }
 
+  async function fetchProfile() {
+    const response = await axios.get(API_URL + "/profile", WITH_CREDENTIALS);
+    setAuth(response.data[0].first_name);
+    setUserId(response.data[0].person_id);
+  }
 
-        const userRole = response.data.user.role_id === 1 ? "Manager" : "Employee" 
-        setRole(userRole);
+  //Homepage component
+  //Logik i navbar med manager / employee
+
+  //
+
+  useEffect(() => {
+    fetchUser();
+    fetchProfile();
+  }, []);
+
+  const handleSignOut = async () => {
+    const response = await axios.get(API_URL + "/logout", WITH_CREDENTIALS);
+
+    if (response.status === 200) {
+      toast.success(response.data.message);
     }
+  };
 
-    
-    async function fetchProfile(){
-        const response = await axios.get(API_URL + "/profile", WITH_CREDENTIALS);
-        setAuth(response.data[0].first_name)
-        setUserId(response.data[0].person_id)
-    }
-
-    
-    //Homepage component
-    //Logik i navbar med manager / employee
-
-    //
-
-
-    useEffect(() => {
-        fetchUser()
-        fetchProfile()
-    }, []);
-
-
-
-    const handleSignOut = async () => {
-        const response = await axios.get(API_URL + "/logout", WITH_CREDENTIALS);
-
-        if (response.status === 200) {
-            toast.success(response.data.message);
-        }
-    };
-
-    return (
+  return (
     <div>
-    <ToastContainer
+      <ToastContainer
         autoClose={3000}
         closeOnClick={true}
         position={toast.POSITION.TOP_CENTER}
         limit={1}
-    />
-    <nav className={Navbar.navContainer}>
+      />
+      <nav className={Navbar.navContainer}>
         <div className={Navbar.logoContainer}>
-        <Link to="/main">
+          <Link to="/main">
             <img className={Navbar.logo} src={logo} alt="Logo" />
+          </Link>
+        </div>
+        {role === "Employee" && (
+          <div className={Navbar.Options}>
+            <Link to="/main" className={Navbar.Link}>
+              <h4>Home</h4>
             </Link>
-        </div>
-{role === "Employee" && (
-    <div className={Navbar.Options}>
-        <Link to="/main" className={Navbar.Link}>
-        <h4>Home</h4>
-        </Link>
-        <Link className={Navbar.Link} to="/projects">
-        <h4>View Projects</h4>
-        </Link>
-        </div>
+            <Link className={Navbar.Link} to="/projects">
+              <h4>View Projects</h4>
+            </Link>
+          </div>
         )}
         {role === "Manager" && (
-    <div className={Navbar.Options}>
-        <Link to="/main" className={Navbar.Link}>
-        <h4>Home</h4>
-        </Link>
-        <Link className={Navbar.Link} to="/projects">
-        <h4>View Projects</h4>
-        </Link>
-        <Link className={Navbar.Link} to="/createProject">
-        <h4>Create Project</h4>
-        </Link>
-        <Link className={Navbar.Link}>
-        <h4>Search Employee Skills</h4>
-        </Link>
-        </div>
+          <div className={Navbar.Options}>
+            <Link to="/main" className={Navbar.Link}>
+              <h4>Home</h4>
+            </Link>
+            <Link className={Navbar.Link} to="/projects">
+              <h4>View Projects</h4>
+            </Link>
+            <Link className={Navbar.Link} to="/createProject">
+              <h4>Create Project</h4>
+            </Link>
+            <Link className={Navbar.Link}>
+              <h4>Search Employee Skills</h4>
+            </Link>
+          </div>
         )}
         <div className={Navbar.iconWrapper}>
-            <li className={Navbar.dropdown}>
-            <h4>{auth} / {role} </h4>
-                <FontAwesomeIcon
-                    icon={faCaretDown}
-                    className={Navbar.dropbtnArrow}
-                    style={{ color: '#A100FF' }}
-                    onClick={() => setDropdownVisible(!dropdownVisible)}
-                />
-                
-                {dropdownVisible && role === "Employee" && (
-                    <div className={Navbar.dropdownContent}>
-                        <Link to="/login" onClick={handleSignOut}>Sign out</Link>
-                        <Link to={`/profile/update/${userId}`}>Update Profile</Link>
-                        
-                    </div>
-                )}
+          <li className={Navbar.dropdown}>
+            <h4>
+              {auth} / {role}
+            </h4>
+            <FontAwesomeIcon
+              icon={faCaretDown}
+              className={Navbar.dropbtnArrow}
+              style={{ color: "#A100FF" }}
+              onClick={() => setDropdownVisible(!dropdownVisible)}
+            />
 
-                {dropdownVisible && role === "Manager" && (
-                    <div className={Navbar.dropdownContent}>
-                        <Link to="/login" onClick={handleSignOut}>Sign out</Link>
-                        <Link to={`/profile/update/${userId}`}>Update Profile</Link>
-                        <Link to={`/profile/update/${userId}`}>Create Manager</Link>
-                        <Link to={""}>View Managers / Employees</Link>
-                    </div>
-                )}
-            </li>
+            {dropdownVisible && role === "Employee" && (
+              <div className={Navbar.dropdownContent}>
+                <Link to="/login" onClick={handleSignOut}>
+                  Sign out
+                </Link>
+                <Link to={`/profile/update/${userId}`}>Update Profile</Link>
+              </div>
+            )}
+
+            {dropdownVisible && role === "Manager" && (
+              <div className={Navbar.dropdownContent}>
+                <Link to="/login" onClick={handleSignOut}>
+                  Sign out
+                </Link>
+                <Link to={`/profile/update/${userId}`}>Update Profile</Link>
+                <Link to={`/profile/update/${userId}`}>Create Manager</Link>
+                <Link to={""}>View Managers / Employees</Link>
+              </div>
+            )}
+          </li>
         </div>
-    </nav>
-</div>
-)
-}
-
+      </nav>
+    </div>
+  );
+};
