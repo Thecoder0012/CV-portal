cd008e7b86d3ef7764dd5716b9c6102ad95dc655
Battals <batt0006@stud.kea.dk>
2023-12-04 19:36:48 +0100

    Background video for main Page

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,17 +1,29 @@
-import { NavigationBar } from "./NavigationBar"
+    import React from 'react';
+    import { NavigationBar } from './NavigationBar';
+    import main from '../styles/mainPage.module.css';
+    import videoSrc from '../videoplayback.mp4';
 
-export const MainPage = () => {
-
-
+    export const MainPage = () => {
     return (
         <div>
-<NavigationBar/>
-        <h1>
-        </h1>
-        
+        <NavigationBar /> 
+        <div className={main.mainDiv}>
+            <video autoPlay loop muted className={main.backgroundVideo}>
+            <source src={videoSrc} type="video/mp4" />
+            Your browser does not support the video tag.
+            </video>
+
+            <div className={main.content}>
+            
+            </div>
         </div>
-    )
+    <div>
 
 
+    </div>
+<h1>
 
-}
+</h1>
+        </div>
+    );
+    };
