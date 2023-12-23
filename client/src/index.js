41d96fe0185d10c59d7ac1259f3dc5bc6de22b16
Battals <batt0006@stud.kea.dk>
2023-12-24 00:41:34 +0100

    missing files to previous commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, useLocation } from "react-router-dom";
+import { NavigationBar } from "./components/main/NavigationBar";
 import App from "./App";
 
+const AppWithNavbar = () => {
+  const [showNavigationBar, setShowNavigationBar] = useState();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/login" || location.pathname === "/register" || location.pathname === "/cvProfile") {
+      setShowNavigationBar(false);
+    } else {
+      setShowNavigationBar(true);
+    }
+  }, [location.pathname]);
+
+  return (
+    <>
+      {showNavigationBar && <NavigationBar />}
+      <App />
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <BrowserRouter>
-    <App />
+    <AppWithNavbar />
   </BrowserRouter>
 );
